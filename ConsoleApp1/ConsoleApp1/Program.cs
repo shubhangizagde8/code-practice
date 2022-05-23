401ed255acf9f77a484fdd8b30a7ff786e76b093
shubhangizagde8 <shubhangizagde8@gmail.com>
2022-05-23 04:08:26 +0100

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WELCOME TO VISUAL STUDIO CODE");
+
+            Console.WriteLine("this is my first program");
+            Console.ReadKey();
+
+
+        }
+    }
+}
