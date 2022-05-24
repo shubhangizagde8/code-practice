a70a9f0b570b82d68f3b93d2bed500a4dae16f7c
shubhangizagde8 <shubhangizagde8@gmail.com>
2022-05-24 06:27:00 +0100

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Class1
+    {
+        static void main1(String[] args)
+        {
+            int k;
+            for (k = 0; k >= 10; k++)
+            {
+                Console.WriteLine("*");
+                Console.ReadLine();
+
+            }
+
+            Console.Write("Enter your choice:");
+
+             int p= Convert.ToInt32(Console.ReadLine());
+          
+            switch (p)
+            {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("TUESDAY");
+                    break;
+                case 3:
+                    Console.WriteLine("WENSDAY");
+                    break;
+                case 4:
+                    Console.WriteLine("THARSDAY");
+                    break;
+                case 5:
+                    Console.WriteLine("FRIDAY");
+                    break;
+                case 6:
+                    Console.WriteLine("SATERDAY");
+                    break;
+                case 7:
+                    Console.WriteLine("SUNDAY");
+                    break;
+
+                   default:
+                    Console.WriteLine("Not AVAIBLE:");
+                    Console.WriteLine("ENTER CHOICE BETWEEN 1 TO 7:");
+                    break;
+            }
+            
+
+            /*  Console.WriteLine("");
+              Console.WriteLine("Enter number 1 :");
+              int a = Convert.ToInt32(Console.ReadLine());
+              Console.WriteLine("");
+
+              Console.WriteLine("Enter number 2:");
+              int b = Convert.ToInt32(Console.ReadLine());
+              Console.WriteLine("");
+
+              Console.WriteLine("1 :Addition");
+              Console.WriteLine("2:subastraction");
+              Console.WriteLine("3:multiplication");
+              Console.WriteLine("4:devision");
+              Console.WriteLine("");
+
+              Console.Write("Enter your choice for perform above operation:");
+              int ch = Convert.ToInt32(Console.ReadLine());
+              Console.WriteLine("");
+
+
+              int r;
+
+              switch (ch)
+              {
+                  case 1:
+                      r = a + b;
+                      Console.WriteLine("Addition is : "+r);
+
+                      break;
+                  case 2:
+                      r = a - b;
+                      Console.WriteLine("subastraction is: "+r);
+                      break;
+                  case 3:
+                      r = a * b;
+                      Console.WriteLine("multiplication is: "+r);
+                      break;
+                  case 4:
+                      r = a / b;
+                      Console.WriteLine("devision is: "+r);
+                      break;
+
+                  default:
+                      Console.WriteLine("Not AVAIBLE:");
+                      Console.WriteLine("ENTER CHOICE BETWEEN 1 TO 4:");
+                      break;
+              }*/
+
+
+
+        
+        }
+
+
+    }
+}
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,10 +7,125 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("WELCOME TO VISUAL STUDIO CODE");
 
-            Console.WriteLine("this is my first program");
+            /*   int k;
+               for (k = 1; k <= 10; k++)
+               {
+                   for (int j = 1; j <= k; j++)
+                   {
+                       Console.Write("*");
+                   }
+                   Console.WriteLine("");
+               }*/
+            /* int k;
+             for (k = 1; k <= 10; k++)
+             {
+                 for (int j = 1; j<=10; j++)
+                 {
+
+                     Console.Write(" ");
+                     if (k == j)
+                     {
+                         Console.Write("*");
+                     }
+                     if (j==1)
+                     {
+                         Console.Write("*");
+                     }
+                    else if(j==10)
+                     {
+                         Console.Write("*");
+
+                     }
+                     else if (k==1 &&   j<8)
+                     {
+                         Console.Write("*");
+
+                     }
+                     else if (k==10 && j<8)
+                     {
+                         Console.Write("*");
+
+                     }
+
+                     else if (k+j==11)
+                     {
+                         Console.Write("*");
+
+                     }
+                     else
+                     {
+                         Console.Write("");
+
+                     }
+                 }
+                     Console.WriteLine("");
+
+             }*/
+
+            int k;
+            for (k = 1; k <= 10; k++)
+            {
+                for (int j = 1; j <= 10; j++)
+                {
+                    if (k < j )
+                    {
+                        Console.Write("*");
+                    }
+                    
+
+                }
+
+                Console.WriteLine("");
+            }
+
+            for (k = 1; k <= 10; k++)
+            {
+                for (int j = 1; j <= 10; j++)
+                {
+
+                    if (k > j)
+                        Console.Write("*");
+                }
+
+                Console.WriteLine("");
+            }
+
+
+
+            /*int b = 10;
+          if (b%2==0)
+          {
+              Console.WriteLine("NUMBER IS EVEN");
+          }
+          else
+              {
+              Console.WriteLine("number is odd");
+          }
+
+
+          int c = 64;
+          if (c > 18)
+          {
+              Console.WriteLine("he can give vote");
+          }
+          if (c > 60)
+          {
+              Console.WriteLine("BUT he is a senior citizen ");
+          }
+          else if (c<18)
+          {
+              Console.WriteLine("he is tin ager ");
+          }
+          else
+          {
+              Console.WriteLine("he can not giev vote");
+          }
+
+          */
+
             Console.ReadKey();
+
 
 
         }
